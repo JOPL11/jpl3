44060d0da85f80f21ac7e5338146c0280f0f617f
unknown <jan.peiro@protonmail.com>
2025-07-29 13:37:20 +0200

    neuLogo

--- app/page.js
+++ app/page.js
@@ -434,7 +434,7 @@ export default function Home() {
                   client={{
                     name: "Sieber & Wolf Werbeagentur  |",
                     website: "https://sieberundwolf.de/",
-                    logo: "/images/agencies/SW_neu_white.png"
+                    logo: "/images/agencies/sw_neu_white.png"
                   }}
                   modalContent={{
                     description: `Logo animation for Mercedes Benz.
