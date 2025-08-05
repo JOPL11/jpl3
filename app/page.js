ee17dc45df028056a6845e192c0b45922ef44321
unknown <jan.peiro@protonmail.com>
2025-08-05 12:44:18 +0200

    ScreeningArrangement

--- app/page.js
+++ app/page.js
@@ -426,8 +426,8 @@ export default function Home() {
                   title="Stadtberichter App"
                   image="/images/corp/sb.jpg"
                   alt="Stadtberichter App"
-                  //link="https://sb202.vercel.app/"
-                  text="<b>Currently in development.</b> A GDPR-compliant civics oriented app with encrypted geodata, voice-to-text, i18n language versioning, rate-limiting and other security features feeding into a real-time municipal dashboard"
+                  //link="https://sb2025.vercel.app/"
+                  text="<b>Currently in development. Screening can be arranged.</b> A GDPR-compliant civics oriented app with encrypted geodata, voice-to-text, i18n language versioning, rate-limiting and other security features feeding into a real-time municipal dashboard"
                 >
                   <p><strong>Target Audience:</strong> Municipal Citizens</p>
                   <p><strong>Project Type:</strong> Citizen Reporting App</p>
