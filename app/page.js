ef0a115558d89210e0818be50cc11cae474e3ae6
unknown <jan.peiro@protonmail.com>
2026-09-22 01:21:34 +0300

    hark2

--- app/page.js
+++ app/page.js
@@ -1403,7 +1403,7 @@ useEffect(() => {
             <h2 style={{paddingTop: "5rem"}}>
               <AnimatedText ref={contactHeadingRef}>Contact</AnimatedText>
             </h2>
-            <div style={{height: '0.1rem', marginBottom: '11rem', fontSize: '1.1rem'}}>Hark! Let thy words descend upon me, as rain upon parched earth </div>
+            <div style={{height: '0.1rem', marginBottom: '11rem', fontSize: '1.1rem'}}>Hark! Let thy words descend upon me, as rain upon parched earth.</div>
               <ContactForm />
             </section>
         </div>
