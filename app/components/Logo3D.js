4ed930863fcd410f3bf1cbf008c633cf1bb90229
unknown <jan.peiro@protonmail.com>
2026-09-22 01:37:42 +0300

    thing

--- app/components/Logo3D.js
+++ app/components/Logo3D.js
@@ -648,8 +648,8 @@ function Scene({ modelUrl }) {
         onHolographicChange={setIsHolographic}
         isHovered={isHovered}
       />
-      <NameText />
-      {/* First cube */}
+      
+      {/* First cube <NameText /> */}
       <OrbitingCube speed={1.3} positionOffset={0} rotationSpeed={1} visible={!isHolographic}/>
       
       {/* Second cube with offset position and different speed */}
